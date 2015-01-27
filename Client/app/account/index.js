66272c6799c3b77eef2fb9e98460dd46f768031f
Tommy Jiang <tianyangj@gmail.com>
2015-01-26 18:50:53 -0800

    add satellizer

--- Client/app/account/index.js
+++ Client/app/account/index.js
@@ -1,1 +1,9 @@
-angular.module('account', ['supersonic']);
+angular.module('account', ['supersonic', 'satellizer'])
+	.config(function($authProvider) {
+
+		$authProvider.facebook({
+			clientId: '139356541211',
+			url: 'http://192.168.1.113:9000/auth/facebook'
+		});
+
+	});
--- Client/app/account/scripts/signinController.js
+++ Client/app/account/scripts/signinController.js
@@ -1,7 +1,16 @@
-angular.module('account').controller('signinController', function($scope, supersonic) {
+angular.module('account').controller('signinController', function($scope, $auth, supersonic) {
 
 	$scope.close = function() {
 		supersonic.ui.modal.hide();
+	};
+
+	$scope.authenticate = function(provider) {
+		supersonic.logger.log(provider);
+		$auth.authenticate(provider).then(function(response) {
+			supersonic.logger.log('signed in');
+		}, function(err) {
+			supersonic.logger.log('auth.authenticate failed');
+		});
 	};
 
 });
